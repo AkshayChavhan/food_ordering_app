f6432c336ad3770b5d61105e893cbde36b4baa21
akshaychavhan <akshay@rtledgers.com>
2023-12-04 09:11:13 +0530

    4_register ui complete

--- src/component/layout/Header.js
+++ src/component/layout/Header.js
@@ -5,17 +5,30 @@ import Link from "next/link";
 function Header() {
   return (
     <header className="flex justify-between">
-    <Link href={''} className="text-primary font-semibold text-2xl">
-      Desi Piza
-    </Link>
-    <nav className="flex gap-8 items-center text-primary">
-      <Link href={''}> Home </Link>
-      <Link href={''}> Menu </Link>
-      <Link href={''}> About </Link>
-      <Link href={''}> Contact </Link>
-      <Link className='bg-primary text-white font-semibold px-6 py-3 rounded-full' href={''}> Login </Link>
-    </nav>
-  </header>
+      <Link href={''} className="text-primary font-semibold text-2xl">
+        Desi Piza
+      </Link>
+      <nav className="flex gap-8 items-center text-primary">
+        <Link href={''}> Home </Link>
+        <Link href={''}> Menu </Link>
+        <Link href={''}> About </Link>
+        <Link href={''}> Contact </Link>
+      </nav>
+      <nav>
+        <Link
+          className='bg-primary text-white font-semibold px-6 py-3 rounded-full'
+          href={'/login'}
+        >
+          Login
+        </Link>
+        <Link
+          className='font-semibold px-6 py-3'
+          href={'/register'}
+        >
+          Register
+        </Link>
+      </nav>
+    </header>
   )
 }
 
